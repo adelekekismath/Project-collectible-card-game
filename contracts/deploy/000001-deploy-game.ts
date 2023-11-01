5995f6d08bc1be4f88b81e821c58e59447889288
Nour TOUNSI <n.tounsi@pathecinemas.com>
2023-11-01 12:57:48 +0100

    harhat integration

--- contracts/deploy/000001-deploy-game.ts
+++ contracts/deploy/000001-deploy-game.ts
@@ -1,23 +1,31 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import { ethers } from "hardhat";
+// /* eslint-disable @typescript-eslint/no-unused-vars */
+// import { ethers } from "hardhat";
 
-async function main() {
-  // const contract = await ethers.deployContract("Main");
-  // await contract.deployed();
-  // console.log("Se deployed to SEPOLIA", contract.address);
-  //Se deployed to SEPOLIA 0x2f4d5Ef6C6dcA8eB102b30E41e5403fF6fd49CA0
-  const contract = await ethers.deployContract("NFTCard");
-  await contract.deployed();
-  console.log("NFT deployed to SEPOLIA", contract.address);
+// async function main() {
+//   // const contract = await ethers.deployContract("Main");
+//   // await contract.deployed();
+//   // console.log("Se deployed to SEPOLIA", contract.address);
+//   //Se deployed to SEPOLIA 0x2f4d5Ef6C6dcA8eB102b30E41e5403fF6fd49CA0
+//   const contract = await ethers.deployContract("NFTCard");
+//   await contract.deployed();
+//   console.log("NFT deployed to SEPOLIA", contract.address);
+// }
+
+
+// main().catch((error) => {
+//   console.error(error);
+//   process.exitCode = 1;
+// });
+import 'dotenv/config'
+import { DeployFunction } from 'hardhat-deploy/types'
+
+const deployer: DeployFunction = async hre => {
+  if (hre.network.config.chainId !== 31337) return
+  const { deployer } = await hre.getNamedAccounts()
+  await hre.deployments.deploy('NFTCard', { from: deployer, log: true })
 }
 
-
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
-
-
+export default deployer
 
 
 
--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,46 +1,83 @@
 
-import '@nomiclabs/hardhat-ethers';
-import '@nomiclabs/hardhat-waffle';
-import '@typechain/hardhat';
-import 'dotenv/config';
-import 'hardhat-abi-exporter';
-import 'hardhat-deploy';
-import 'hardhat-gas-reporter';
+// import '@nomiclabs/hardhat-ethers';
+// import '@nomiclabs/hardhat-waffle';
+// import '@typechain/hardhat';
+// import 'dotenv/config';
+// import 'hardhat-abi-exporter';
+// import 'hardhat-deploy';
+// import 'hardhat-gas-reporter';
 
-const { API_KEY_ETHER, SEPOLIA_PRIVATE_KEY, ALCHEMY_API_KEY } = process.env;
+// const { API_KEY_ETHER, SEPOLIA_PRIVATE_KEY, ALCHEMY_API_KEY } = process.env;
 
-// Assurer que les variables d'environnement requises sont définies
-if (!ALCHEMY_API_KEY || !API_KEY_ETHER || !SEPOLIA_PRIVATE_KEY) {
-  throw new Error('One or more required environment variables are missing.');
-}
-// module.exports = {
+// // Assurer que les variables d'environnement requises sont définies
+// if (!ALCHEMY_API_KEY || !API_KEY_ETHER || !SEPOLIA_PRIVATE_KEY) {
+//   throw new Error('One or more required environment variables are missing.');
+// }
+// // module.exports = {
+// //   solidity: '0.8.20',
+// //   defaultNetwork:"sepolia",
+// //   networks: {
+// //      hardhat: {},
+// //      sepolia: {
+// //       url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+// //       accounts: [SEPOLIA_PRIVATE_KEY],
+// //       gas: 210000000,
+// //       gasPrice: 800000000000,
+// //     },
+// // }
+// // }
+// import { HardhatUserConfig } from "hardhat/config";
+// const config: HardhatUserConfig = {
 //   solidity: '0.8.20',
 //   defaultNetwork:"sepolia",
+//   paths: {
+//     deploy: './deploy',
+//     sources: './src',
+//   },
 //   networks: {
-//      hardhat: {},
-//      sepolia: {
+//     sepolia: {
 //       url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
 //       accounts: [SEPOLIA_PRIVATE_KEY],
 //       gas: 210000000,
-//       gasPrice: 800000000000,
+//       gasPrice: 1500000256,
 //     },
-// }
-// }
-import { HardhatUserConfig } from "hardhat/config";
+//   },
+//   namedAccounts: {
+//     deployer: { default: 0 },
+//     admin: { default: 0 },
+//     second: { default: 1 },
+//     random: { default: 8 },
+//   },
+//   abiExporter: {
+//     runOnCompile: true,
+//     path: '../frontend/src/abis',
+//     clear: true,
+//     flat: true,
+//     only: [],
+//     pretty: true,
+//   },
+//   typechain: {
+//     outDir: '../typechain',
+//   },
+// };
+
+// export default config;
+import '@nomiclabs/hardhat-ethers'
+import '@nomiclabs/hardhat-waffle'
+import '@typechain/hardhat'
+import 'dotenv/config'
+import 'hardhat-abi-exporter'
+import 'hardhat-deploy'
+import 'hardhat-gas-reporter'
+import { HardhatUserConfig } from 'hardhat/config'
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
 const config: HardhatUserConfig = {
   solidity: '0.8.20',
-  defaultNetwork:"sepolia",
   paths: {
     deploy: './deploy',
     sources: './src',
-  },
-  networks: {
-    sepolia: {
-      url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
-      accounts: [SEPOLIA_PRIVATE_KEY],
-      gas: 210000000,
-      gasPrice: 1500000256,
-    },
   },
   namedAccounts: {
     deployer: { default: 0 },
@@ -59,6 +96,6 @@ const config: HardhatUserConfig = {
   typechain: {
     outDir: '../typechain',
   },
-};
+}
 
-export default config;
+export default config
